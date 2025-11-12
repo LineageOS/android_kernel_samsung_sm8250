06c63080dc9e4a245cb4fad93adfd2f57b292872
ata-kaner <ata.kaner03@gmail.com>
2025-11-12 07:04:20 +0300

    fs: fuse: Clear Samsung changes
    For reducing conflicts on upcoming merge
    
    Change-Id: I0055c733259e0f1b58cf7831be7827b41bb0bd1d

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -116,9 +116,6 @@
  *
  *  7.27
  *  - add FUSE_ABORT_ERROR
- * 
- *  7.28
- *  - add FUSE_MAX_PAGES, add max_pages to init_out
  */
 
 #ifndef _LINUX_FUSE_H
@@ -254,7 +251,6 @@ struct fuse_file_lock {
  * FUSE_HANDLE_KILLPRIV: fs handles killing suid/sgid/cap on write/chown/trunc
  * FUSE_POSIX_ACL: filesystem supports posix acls
  * FUSE_ABORT_ERROR: reading the device after abort returns ECONNABORTED
- * FUSE_MAX_PAGES: init_out.max_pages contains the max number of req pages
  */
 #define FUSE_ASYNC_READ		(1 << 0)
 #define FUSE_POSIX_LOCKS	(1 << 1)
@@ -278,7 +274,6 @@ struct fuse_file_lock {
 #define FUSE_HANDLE_KILLPRIV	(1 << 19)
 #define FUSE_POSIX_ACL		(1 << 20)
 #define FUSE_ABORT_ERROR	(1 << 21)
-#define FUSE_MAX_PAGES		(1 << 22)
 #define FUSE_PASSTHROUGH	(1 << 31)
 
 /**
@@ -619,9 +614,7 @@ struct fuse_init_out {
 	uint16_t	congestion_threshold;
 	uint32_t	max_write;
 	uint32_t	time_gran;
-	uint16_t	max_pages;
-	uint16_t	padding;
-	uint32_t	unused[8];
+	uint32_t	unused[9];
 };
 
 #define CUSE_INIT_INFO_MAX 4096
