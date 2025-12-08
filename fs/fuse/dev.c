5a3383bc9bf2894e01d2842e35ee101ef95f0f97
Bruno Martins <bgcngm@gmail.com>
2025-12-08 22:55:44 +0000

    fs: fuse: Remove SEC modifications
    Change-Id: I583595478d575f0288df2fad9094e27ce9df510a

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -2179,10 +2179,6 @@ void fuse_abort_conn(struct fuse_conn *fc)
 {
 	struct fuse_iqueue *fiq = &fc->iq;
 
-	/* @fs.sec -- d7bd5cc97a05d48e04defc719fbaffefdd4e6f22 -- */
-	ST_LOG("<%s> dev = %u:%u  fuse abort all requests",
-			__func__, MAJOR(fc->dev), MINOR(fc->dev));
-
 	spin_lock(&fc->lock);
 	if (fc->connected) {
 		struct fuse_dev *fud;
